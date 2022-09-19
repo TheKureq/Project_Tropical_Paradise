148674a891a7d07eb41426de33a8eab1790d95c4
TheKureq <81649868+TheKureq@users.noreply.github.com>
2022-09-20 00:44:10 +0200

    finished website v1

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
 
 
 
@@ -25,5 +29,25 @@ const handleNavItemsAnimation = () => {
     })
 }
 
+const handleCurrentYear = () => {
+    const year = (new Date).getFullYear();
+    footerYear.innerText = year;
+}
 
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+
+    allSections.forEach(section => {
+
+        if (section.classList.contains('white-section') && (section.offsetTop <= currentSection + 60)) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && (section.offsetTop <= currentSection + 60)) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
+
+    })
+}
+
+handleCurrentYear()
+window.addEventListener('scroll', handleObserver)
 navBtn.addEventListener('click', handleNav)
